a9a85559e091174b5fb88f54a3e5905cc47ac5b8
Benson Pan <benson.pan13@gmail.com>
2016-06-02 20:52:44 -0400

    initial commit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,109 @@
 'use strict';
-
 /* Controller */
-angular.module('myApp.controllers', []).
+angular.module('myApp', []).
 	controller('AppCtrl', function ($scope, $http) {
-		// Insert controller code here
-		
+		// variables used
+		$scope.apparel;
+		$scope.price;
+
+		// ng-model variables
+		$scope.selectedStyle;
+		$scope.selectedColor;
+		$scope.selectedSize;
+		$scope.quantity;
+		$scope.errorMsg;
+		$scope.total;
+
+		// stores formatted color and size values to be more readable
+		$scope.mappedColor;
+		$scope.mappedSize;
+
+		$http.get('/api/apparel/:styleCode?').then(function(apparel, status) {
+			$scope.apparel = apparel.data;
+		});
+		$scope.submitForm = function(){
+			$scope.total ='';
+			var errors=[];
+
+			if ($scope.selectedStyle === undefined)
+				errors.push('style')
+			else{// clean up code here
+				if ($scope.selectedColor !== undefined){
+					if (!$scope.selectedStyle.color_codes.includes($scope.selectedColor
+					                                               .substring($scope.selectedColor.indexOf(':') + 1).toUpperCase()))
+						errors.push('color');
+				}
+				else
+					errors.push('color');
+
+				if (!$scope.selectedStyle.size_codes.includes($scope.selectedSize))
+					errors.push('size');
+
+				if ($scope.quantity	=== undefined)
+					errors.push('quantity');
+			}
+
+
+			if (errors.length){
+				$scope.errorMsg = 'Please select valid values for ' + errors.join(', ') + '.';
+			}
+			else
+			{
+				$scope.errorMsg = null;
+				var data = {'sr': $scope.selectedStyle.style_code,
+				'cc':$scope.selectedColor.substring(0, $scope.selectedColor.indexOf(':')),
+				'sc':$scope.selectedSize.substring(0, $scope.selectedSize.indexOf(':'))};
+
+				$http.post('/api/quote/',data).then(function(price, status) {
+					$scope.price = Number(price.data);
+
+					var weight = Number($scope.selectedStyle.weight);
+					var shippingCost = 0;
+					var cost = $scope.price * $scope.quantity;
+					var markup = 0;
+					var finalWShipping = 0;
+					var finalWCompensation = 0;
+
+					if (weight >= 0.4){
+						if ($scope.quantity < 48)
+							shippingCost = $scope.quantity;
+						else
+							shippingCost = 0.75 * $scope.quantity;
+					}
+					else{
+						if ($scope.quantity < 45)
+							shippingCost = 0.5 * $scope.quantity;
+						else
+							shippingCost = 0.25 * $scope.quantity;
+					}
+
+					markup = (cost < 800 ? cost * 1.5 : cost * 1.45);
+					finalWShipping = markup	+ shippingCost;
+					finalWCompensation = finalWShipping * 1.07;
+
+					// debug values below
+					/*
+					console.log('cost: ' + cost.toFixed(2) +
+					'\nmarkup cost: ' + markup.toFixed(2) +
+					'\nweight: ' + weight +
+					'\nshippingCost: ' + shippingCost.toFixed(2) +
+					'\nfinal cost w shipping: ' + finalWShipping.toFixed(2) +
+					'\nfinal cost w compensation: ' + finalWCompensation.toFixed(2) +
+					'\ncustomer unit price = ' + (finalWCompensation/$scope.quantity).toFixed(2));
+					*/
+					$scope.total = "Total cost of this order is $" + finalWCompensation.toFixed(2) +
+					".\n Individual cost is $" + (finalWCompensation / $scope.quantity).toFixed(2) +
+					"/item.";
+				});
+			}
+		}
+		$scope.updateFields = function(){
+			$scope.mappedColor=$scope.selectedStyle.color_codes.split(';').sort()
+			.map(x => x.substr(0,x.indexOf(':') + 1) + x.substring(x.indexOf(':', 3))[1].toUpperCase()
+			     +x.substring(x.indexOf(':', 3) + 2).toLowerCase());
+
+			$scope.mappedSize=$scope.selectedStyle.size_codes.split(';').sort()
+		}
 	});
+
+
--- routes/api.js
+++ routes/api.js
@@ -10,28 +10,44 @@ var db          = require('../config/db').connection;
 // API endpoint for /api/apparel
 api.get('/api/apparel/:styleCode?', function(req, res) {
 	// Insert Apparel API code here
-
+	db.query('SELECT DISTINCT * from apparel group by style_code', function(err, rows, fields) {
+		if (err) throw err;
+		res.json(rows);
+	});
 });
 
 // API endpoint for /api/quote
 api.post('/api/quote', function(req, res) {
-	// Insert Quoting API code here
-
+	// console.log(req.body.sr, req.body.cc, req.body.sc);
+	var apparelPrice = getApparelPrice(req.body.sr, req.body.cc, req.body.sc)
+	.then(function(value){
+		res.send(value);
+	});
 });
 
 // Function for making an Inventory API call
 var getApparelPrice = function getPrice(style_code, color_code, size_code) {
 	var	apparelPriceDeferred = q.defer();
-
 	// Format the Inventory API endpoint as explained in the documentation
-	https.get('// INSERT INVENTORY API END POINT', function(res) {
+	var link = 'https://www.alphashirt.com/cgi-bin/online/xml/inv-request.w?sr='+
+	          style_code+'&cc='+color_code+'&sc='+size_code+
+	          '&pr=y&zp=10002&userName=triggered1111&password=triggered2222';
+
+	//console.log("Accessing: " + link); // verify link
+
+	https.get(link, function(res) {
 		res.on('data', function (data) {
 			// Parse response XML data here
-
+			var response = data.toString();
+			var price;
+			if (response.indexOf(' price="') != -1)
+				price = response.substring(response.indexOf(' price="') + 9,
+				                           response.indexOf('"',response.indexOf(' price="') + 8));
+			apparelPriceDeferred.resolve(price);
 		});
 	}).on('error', function(error) {
 		// Handle EDI call errors here
-
+		apparelPriceDeferred.reject('unable to retrieve');
 	});
 
 	return apparelPriceDeferred.promise;
